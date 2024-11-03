bc7620c78812e082722a8d9bd412e92378869efe
Edmond Mbadu <mbadungoma@gmail.com>
2024-11-02 22:43:00 -0700

    set a dynamic way to set location. still in the testing phase

--- src/app/shrink/employee-page/employee-page.component.ts
+++ src/app/shrink/employee-page/employee-page.component.ts
@@ -29,7 +29,7 @@ export class EmployeePageComponent implements OnInit {
 
   currentLat: number = 0;
   currentLng: number = 0;
-  radius = 5; //Set your desired radius in meters
+  radius = 1; //Set your desired radius in meters
 
   displayBonus: boolean = false;
   displayPayment: boolean = false;
